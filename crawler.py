2cb1c1dae2cc3d1f73593b4254364be6914fa540
ShiZhan <g.shizhan.g@gmail.com>
2012-11-13 17:13:50 +0800

    split crawler and data mapper dict

--- crawler.py
+++ crawler.py
@@ -11,40 +11,25 @@
 #!/usr/bin/env python
 
 import urllib2
+import json
+import re
 from bs4 import BeautifulSoup
 
-sites = [
-    {
-        'name' : 'OSDI 2012', # use local page for test
-        # 'url' : "https://www.usenix.org/conference/osdi12/tech-schedule/osdi-12-program",
-        'url' : "file:///E:/Works/hive/pub-owl.collector/.testpage/osdi2012.htm",
-        'node' : 'h2',
-        'attrs' : {'class' : 'node-title clearfix'},
-    },
-    {
-        'name' : 'WWW 2011',
-        # 'url' : "http://www.www2011india.com/proceeding/forms/pcontents.htm",
-        'url' : "file:///e:/Works/hive/pub-owl.collector/.testpage/www2011.htm",
-        'node' : 'font',
-        'attrs' : {'color' : '#177EB9', 'face' : 'Arial', 'size' : '2'}
-    },
-    {
-        'name' : 'SOSP 2011',
-        # 'url' : "http://sigops.org/sosp/sosp11/current/index.html",
-        'url' : "file:///E:/Works/hive/pub-owl.collector/.testpage/sosp2011.html",
-        'node' : 'div',
-        'attrs' : {'class' : 'paperTitle'}
-    }
-]
-
 def main():
+    try:
+        sites_file_raw = open('sites.json', mode='r').read()
+        sites_no_comments = re.sub(r'//##.*', r'', sites_file_raw)
+        sites = json.loads(sites_no_comments)
+    except Exception, e:
+        raise e
+ 
     for site in sites:
         print "Loading site: ", site['name']
         page = urllib2.urlopen(site['url']).read()
         print "Read ", len(page), " Bytes from page: ", site['url']
         soup = BeautifulSoup(page)
         for node_title in soup.findAll(site['node'], attrs = site['attrs']):
-            print node_title
+            print node_title.prettify()
 
 if __name__ == '__main__':
     main()
