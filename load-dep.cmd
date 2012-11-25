76c35f3757e681d099e5fffaec36a30f1a3d3957
ShiZhan <g.shizhan.g@gmail.com>
2012-11-25 12:02:12 +0800

    clean test scripts.

--- load-dep.cmd
+++
@@ -1,1 +0,0 @@
-@echo Created by Shi.Zhan @ 2012&&@echo setting python libpath for scrapy:&&@echo scrapy (twisted (pyopenssl, zope.interface), w3lib)&&@set PYTHONPATH=e:\Works\hive\pyopenssl;e:\Works\hive\zope.interface.git\src;e:\Works\hive\twisted.git;e:\Works\hive\w3lib.git;e:\Works\hive\scrapy.git;%PYTHONPATH%&&
